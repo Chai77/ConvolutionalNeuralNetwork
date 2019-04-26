7cd4983abbb2e24902f6c525037d303acc8ae58e
Chaitanya E <chaitanyaeranki@y7mail.com>
2019-04-26 08:04:08 -0500

    Added multiply method

--- src/com/chaitanya/matrix/Matrix.java
+++ src/com/chaitanya/matrix/Matrix.java
@@ -45,4 +45,35 @@ public class Matrix {
         this.cols = matrix[0][0].length;
     }
 
+    public void multiply(Matrix mat) {
+        assert(mat.getChannels() == 1);
+        assert(channels == 1);
+        assert(cols == mat.getRows());
+        double[][][] result = new double[channels][rows][mat.getCols()];
+        double[][][] matArr = mat.getMatrix();
+        for(int i = 0; i < result.length; i++) {
+            for(int k = 0; k < result[i].length; k++) {
+                for(int j = 0; j < result[i][k].length; j++) {
+                    double sum = 0;
+                    for(int m = 0; m < cols; m++) {
+                        sum += matrix[i][k][m] * matArr[i][m][j];
+                    }
+                    result[i][k][j] = sum;
+                }
+            }
+        }
+        setMatrix(result);
+    }
+
+    public void multiply(double num) {
+        double[][][] result = new double[this.getChannels()][this.getRows()][this.getCols()];
+        for(int i = 0; i < matrix.length; i++) {
+            for(int k = 0; k < matrix[i].length; k++) {
+                for(int j = 0; j < matrix[i][k].length; j++) {
+                    result[i][k][j] = matrix[i][k][j] * num;
+                }
+            }
+        }
+        setMatrix(result);
+    }
 }
--- src/com/chaitanya/matrix/MatrixOperations.java
+++ src/com/chaitanya/matrix/MatrixOperations.java
@@ -4,4 +4,38 @@ package com.chaitanya.matrix;
 */
 public class MatrixOperations {
     private MatrixOperations() {}
+
+    public static Matrix multiply(Matrix mat1, Matrix mat2) {
+        assert(mat1.getChannels() == mat2.getChannels());
+        assert(mat1.getChannels() == 1);
+        assert(mat1.getCols() == mat2.getRows());
+        double[][][] result = new double[mat1.getChannels()][mat1.getRows()][mat2.getCols()];
+        double[][][] mat1Arr = mat1.getMatrix();
+        double[][][] mat2Arr = mat2.getMatrix();
+        for(int i = 0; i < result.length; i++) {
+            for(int k = 0; k < result[i].length; k++) {
+                for(int j = 0; j < result[i][k].length; j++) {
+                    double sum = 0;
+                    for(int m = 0; m < mat1.getCols(); m++) {
+                        sum += mat1Arr[i][k][m] * mat2Arr[i][m][j];
+                    }
+                    result[i][k][j] = sum;
+                }
+            }
+        }
+        return new Matrix(result);
+    }
+
+    public static Matrix multiply(Matrix mat, double num) {
+        double[][][] result = new double[mat.getChannels()][mat.getRows()][mat.getCols()];
+        double[][][] matrix = mat.getMatrix();
+        for(int i = 0; i < matrix.length; i++) {
+            for(int k = 0; k < matrix[i].length; k++) {
+                for(int j = 0; j < matrix[i][k].length; j++) {
+                    result[i][k][j] = matrix[i][k][j] * num;
+                }
+            }
+        }
+        return new Matrix(result);
+    }
 }
