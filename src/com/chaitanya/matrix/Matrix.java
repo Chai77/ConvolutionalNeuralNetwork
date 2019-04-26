5d1fe9c9aaf01511a9bc2e49a06a2e1260e5aa9a
Chaitanya E <chaitanyaeranki@y7mail.com>
2019-04-25 23:11:39 -0500

    adding basic matrix functions

--- src/com/chaitanya/matrix/Matrix.java
+++ src/com/chaitanya/matrix/Matrix.java
@@ -5,4 +5,44 @@ package com.chaitanya.matrix;
 */
 public class Matrix {
     
+    private double[][][] matrix;
+    private int rows, cols, channels;
+
+    public Matrix(int rows, int cols, int channels) {
+        this.rows = rows;
+        this.cols = cols;
+        this.channels = channels;
+        this.matrix = new double[channels][rows][cols];
+    }
+
+    public Matrix(double[][][] matrix) {
+        this.matrix = matrix;
+        this.channels = matrix.length;
+        this.rows = matrix[0].length;
+        this.cols = matrix[0][0].length;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getChannels() {
+        return channels;
+    }
+
+    public double[][][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(double[][][] matrix) {
+        this.matrix = matrix;
+        this.channels = matrix.length;
+        this.rows = matrix[0].length;
+        this.cols = matrix[0][0].length;
+    }
+
 }
